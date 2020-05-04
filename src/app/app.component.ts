83726b3150b2cbf5248d2241d85ac9637a45f53d
habibahassan <halimaadan92@gmail.com>
2020-05-04 21:13:11 +0300

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: `
+    <game-board></game-board>
+  `,
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'ng-tetris';
+}
+
+
+
+
---
+++ src/app/app.module.ts
@@ -0,0 +1,19 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BoardComponent } from './board.component';
+
+@NgModule({
+  declarations: [
+    AppComponent , BoardComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
